b88165431979963e08c5e95081dcaaaa5cdc57d4
Karthi Thyagarajan <karthit@google.com>
2024-03-05 06:54:45 -0600

    Added license headers

--- spanner-data-validator-java/src/main/java/com/google/migration/dto/ShardSpecJsonDef.java
+++ spanner-data-validator-java/src/main/java/com/google/migration/dto/ShardSpecJsonDef.java
@@ -1,14 +1,25 @@
+/*
+ Copyright 2024 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 package com.google.migration.dto;
 
-import com.google.migration.TableSpecList;
 import java.io.File;
 import java.io.InputStream;
-import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
 import org.apache.commons.io.FileUtils;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- spanner-data-validator-java/src/test/java/com/google/PartitionRangeTest.java
+++ spanner-data-validator-java/src/test/java/com/google/PartitionRangeTest.java
@@ -1,3 +1,19 @@
+/*
+ Copyright 2024 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 package com.google;
 
 import static org.junit.Assert.assertEquals;
@@ -12,7 +28,6 @@ import com.google.migration.partitioning.PartitionRangeListFetcherFactory;
 import com.google.migration.partitioning.UUIDHelpers;
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.sql.Timestamp;
 import java.util.List;
 import java.util.UUID;
 import org.junit.Test;
